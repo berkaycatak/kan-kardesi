94ed8332295563f79658b3085d87311cd960359f
Berkay Çatak <34205493+berkaycatak@users.noreply.github.com>
2025-01-13 12:58:00 +0300

    karşılama sayfası tasarımı

--- lib/screens/auth/welcome/welcome_screen.dart
+++ lib/screens/auth/welcome/welcome_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:kan_kardesi/screens/auth/welcome/welcome_mixin.dart';
+import 'package:kan_kardesi/style/theme/custom_theme.dart';
+import 'package:kan_kardesi/utils/constants/image/image_constants.dart';
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
@@ -7,9 +11,52 @@ class WelcomeScreen extends StatefulWidget {
   State<WelcomeScreen> createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen> {
+class _WelcomeScreenState extends State<WelcomeScreen> with WelcomeMixin {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return PlatformScaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(
+            left: CustomTheme.screenPadding.left,
+            right: CustomTheme.screenPadding.right,
+            bottom: CustomTheme.screenPadding.bottom,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(ImageConstants.welcome),
+              Column(
+                children: [
+                  Text(
+                    "Bağışçı Ol!\nBağışçılara Ulaş!",
+                    style: Theme.of(context).textTheme.titleLarge,
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    "Çevrende kan bağışına mı ihtiyaç var?\nBağışçıları gör, talebini paylaş ve insanlara ulaş!",
+                    style: Theme.of(context).textTheme.bodyMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  PlatformElevatedButton(
+                    onPressed: () => register(context),
+                    child: const Text("Kayıt Ol"),
+                  ),
+                  PlatformTextButton(
+                    onPressed: () => login(context),
+                    child: const Text("Giriş Yap"),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
