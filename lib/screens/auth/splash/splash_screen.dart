1fd5fef7a5f2cffabbb90725e934a5f0343486a2
Berkay Çatak <34205493+berkaycatak@users.noreply.github.com>
2025-01-13 12:45:34 +0300

    Splash sayfa tasarımı tamamlandı.

--- lib/screens/auth/splash/splash_screen.dart
+++ lib/screens/auth/splash/splash_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:kan_kardesi/screens/auth/splash/splash_mixin.dart';
+import 'package:kan_kardesi/utils/constants/image/svg_constants.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -7,9 +11,40 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with SingleTickerProviderStateMixin, SplashMixin {
+  @override
+  void initState() {
+    init(this, context);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return PlatformScaffold(
+      body: Center(
+        child: AnimatedBuilder(
+          animation: scaleAnimation,
+          builder: (context, child) {
+            return Transform.scale(
+              scale: scaleAnimation.value,
+              child: child,
+            );
+          },
+          child: SizedBox(
+            width: 100,
+            child: SvgPicture.asset(
+              SvgConstants.icon,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
